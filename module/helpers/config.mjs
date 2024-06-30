7b06d9ea0cbe235e8fbafefb0408a769a84419e2
PN <paulng91@gmail.com>
2024-06-30 15:15:34 -0700

    Initial commit from boilerplate

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const PANIC_AT_THE_DOJO = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+PANIC_AT_THE_DOJO.abilities = {
+  str: 'PANIC_AT_THE_DOJO.Ability.Str.long',
+  dex: 'PANIC_AT_THE_DOJO.Ability.Dex.long',
+  con: 'PANIC_AT_THE_DOJO.Ability.Con.long',
+  int: 'PANIC_AT_THE_DOJO.Ability.Int.long',
+  wis: 'PANIC_AT_THE_DOJO.Ability.Wis.long',
+  cha: 'PANIC_AT_THE_DOJO.Ability.Cha.long',
+};
+
+PANIC_AT_THE_DOJO.abilityAbbreviations = {
+  str: 'PANIC_AT_THE_DOJO.Ability.Str.abbr',
+  dex: 'PANIC_AT_THE_DOJO.Ability.Dex.abbr',
+  con: 'PANIC_AT_THE_DOJO.Ability.Con.abbr',
+  int: 'PANIC_AT_THE_DOJO.Ability.Int.abbr',
+  wis: 'PANIC_AT_THE_DOJO.Ability.Wis.abbr',
+  cha: 'PANIC_AT_THE_DOJO.Ability.Cha.abbr',
+};
